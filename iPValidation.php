4394cd03e46337e97b464c56ef1b8162da053b49
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-07-26 15:21:14 -0500

    refactor successful

--- iPValidation.php
+++ iPValidation.php
@@ -5,12 +5,11 @@ function isValidIP(string $str): bool{
         return false;
     }
     foreach($arr as $num){
-        if(!preg_match('/\d/', $num)){
+        if(preg_match('/[^\d]/', $num)){
             return false;
         }
-        if(preg_match('/\s/',$num))
         $int= intval($num);
-            if($num[0]=="0"&&$int>0){
+        if($num[0]=="0"&&$int>0){
                 return false;
             }
         if($int<0||$int>255){
