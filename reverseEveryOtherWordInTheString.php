e1267f04a349cb2999af7f693d4821c661936561
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-07-26 12:23:54 -0500

    reverse is a challenge; psyche; remembering the $ is the challenge

--- reverseEveryOtherWordInTheString.php
+++ reverseEveryOtherWordInTheString.php
@@ -1,12 +1,13 @@
 <?php
 
 function reverse($str) {
- $array=explode(" ",$str);
- $len= count($array);
- for ($i=1; $i<len; $i+=2){
-     $array[$i]= strrev($array[$i]);
- }
- return implode(" ", $array);
-}
+    $str=trim($str);
+   $array=explode(" ",$str);
+   $len= count($array);
+   for ($i=1; $i<$len; $i+=2){
+       $array[$i]= strrev($array[$i]);
+   }
+   return implode(" ", $array);
+  }
 
 echo reverse("the lonely dog is pissed");
