0a8b678018b6a033694ad98bb16cae36632de80e
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-08-02 13:49:33 -0500

    refactored exponentiation to array reduction

--- reverseOrRotate.php
+++ reverseOrRotate.php
@@ -12,19 +12,35 @@ function revRot($str, $sz) {
         return "";
     }
     $chunks= str_split($str, $sz);
+    $processed=[];
     foreach($chunks as $chunk){
-        testChunk($chunk);
+        if(testChunk($chunk)){
+           array_push($processed, strrev($chunk)); // reverse string
+        }else{
+            // rotate string
+        }
     }
+    prettyArray($processed);
     return $chunks;
 }
-
+function sum($carry, $new){
+    return $carry+=$new;
+}
+// echo(array_reduce([1,2,3,4], $sum));
 function testChunk($chunk){
-    $digits= str_split($chunk);
-    $sum=0;
-    foreach($digits as $digit){
-        $num= intval($digit);
-        $sum+=$num**2;
-    }
+    // because any integer exponentiation of an integer preserves its parity
+    $array= str_split($chunk);
+    echo $array;
+    $sum = array_reduce($array, "sum");
+
+    // $digits= str_split($chunk);
+
+    // $sum=0;
+    // foreach($digits as $digit){
+
+    //     $num= intval($digit);
+    //     // $sum+=$num**3;
+    // }
     echo "$chunk:$sum\n";
     // return value of true indicates reverse chunk
     if($sum%2==0){return true;}else{return false;}
