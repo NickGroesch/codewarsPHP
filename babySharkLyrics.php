5d288e04a7a16450554de7a0b057cdb4af2e1b86
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-08-02 15:40:43 -0500

    cannot seem to get the actual ellipsis character in php, but code is short enough now

--- babySharkLyrics.php
+++ babySharkLyrics.php
@@ -1,14 +1,20 @@
 <?php
 
 function babySharkLyrics() {
-    $lyrics="";
-    $prefixes=["Baby shark", "Mommy shark", "Daddy shark", "Grandma shark", "Grandpa shark", "Let's go hunt"];
-    $suffixes=[", doo doo doo doo doo doo\n",", doo doo doo doo doo doo\n",", doo doo doo doo doo doo\n","!\n"];
-    foreach($prefixes as $prefix){
-        foreach($suffixes as $suffix){
-            $lyrics.= $prefix.$suffix;
+    $a="";
+    $x=["Baby", "Mommy", "Daddy", "Grandma", "Grandpa"];
+    for($i=0; $i<6; $i++){
+        for($j=0; $j<4; $j++){
+            $p=$i<5?$x[$i]." shark": "Let's go hunt";
+            $s=$j<3?", doo doo doo doo doo doo\n":"!\n";
+            $a.=$p.$s;
         }
     }
-    return $lyrics.="Run away,...";
+    $e='\u2026';
+    return $a.="Run away,$e";
 }
-echo(babySharkLyrics());
+// echo(babySharkLyrics());
+
+echo utf8_encode("U+2026");
+
+// function babySharkLyrics() {$a="";$x=["Baby", "Mommy", "Daddy", "Grandma", "Grandpa"];for($i=0; $i<6; $i++){for($j=0; $j<4; $j++){$p=$i<5?$x[$i]." shark": "Let's go hunt";$s=$j<3?", doo doo doo doo doo doo\n":"!\n";$a.=$p.$s;}}$e='\u2026';return $a.="Run away,$e";}
